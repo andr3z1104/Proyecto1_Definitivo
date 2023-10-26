6dc0da471d96106b7ab04f0f4fbf246139b1f53b
JuanDiaz2004 <ju.diaz@correo.unimet.edu.ve>
2023-10-25 20:06:58 -0400

    UIs

---
+++ UIs/IniciarInterfaz.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package UIs;
+
+/**
+ *
+ * @author ACER
+ */
+public class IniciarInterfaz {
+
+    public static void main(String[] args) {
+        InterfazGeneral ventana = new InterfazGeneral();
+        ventana.setVisible(true);
+        
+    }
+}
